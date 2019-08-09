f32330788c2077767964b8c39bff3b33cd2d68b0
tian <t18215595124@gmail.com>
2019-08-09 11:12:31 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -20,7 +20,8 @@ class SmartWall(Dataset):
         self.label = []
         for i in range(len(file_list)):
             data_raw = dict(np.load(data_path+'/'+file_list[i],allow_pickle=True))['arr_0']
-            for j in range(len(data_raw)):         
+            for j in range(len(data_raw)):   
+                print(data_raw)      
                 self.data.append(data_raw[j]['data'])
                 self.label.append(data_raw[j]['label'])
         self.data_len=len(self.data)
